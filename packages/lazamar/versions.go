7b9262cc23403cd639490f43fc0d03e2415817c0
Victor Borja <vborja@apache.org>
2025-03-19 14:57:57 -0600

    Show help when given an non existing attribute name

--- packages/lazamar/versions.go
+++ packages/lazamar/versions.go
@@ -2,6 +2,7 @@ package lazamar
 
 import (
 	"context"
+	"fmt"
 	"net/url"
 	"strings"
 
@@ -10,7 +11,7 @@ import (
 	lib "github.com/vic/nix-versions/packages/versions"
 )
 
-func Versions(packageName string, channel string) ([]lib.Version, error) {
+func Versions(name string, channel string) ([]lib.Version, error) {
 	var (
 		body   string
 		result []lib.Version
@@ -18,17 +19,20 @@ func Versions(packageName string, channel string) ([]lib.Version, error) {
 	err := requests.
 		URL("https://lazamar.co.uk/nix-versions/").
 		Param("channel", channel).
-		Param("package", packageName).
+		Param("package", name).
 		ToString(&body).
 		Fetch(context.Background())
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Error fetching versions from lazamar.co.uk for `%s`: %v\nPerhaps the package is not available on nixpkgs under the `%s` name.\nTry using https://search.nixos.org/packages?query=%s to find the proper attribute name.", name, err, name, name)
 	}
 	doc, err := htmlquery.Parse(strings.NewReader(body))
 	if err != nil {
 		return nil, err
 	}
 	list := htmlquery.Find(doc, "/html/body/section/table/tbody/tr/td/a/@href")
+	if len(list) == 0 {
+		return nil, fmt.Errorf("No versions found on lazamar.co.uk for `%s`.\nPerhaps the package is not available on nixpkgs under the `%s` name.\nTry using https://search.nixos.org/packages?query=%s to find the proper attribute name.", name, name, name)
+	}
 	for _, link := range list {
 		href, err := url.Parse(htmlquery.InnerText(link))
 		if err != nil {
--- packages/nixhub/versions.go
+++ packages/nixhub/versions.go
@@ -2,6 +2,7 @@ package nixhub
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/carlmjohnson/requests"
 	lib "github.com/vic/nix-versions/packages/versions"
@@ -34,7 +35,7 @@ func Versions(name string) ([]lib.Version, error) {
 		ToJSON(&body).
 		Fetch(context.Background())
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Error fetching versions from nixhub.io for `%s`: %v\nPerhaps the package is not available on nixhub.io under the `%s` name.\nTry using https://www.nixhub.io/search?q=%s to find the proper attribute name.", name, err, name, name)
 	}
 	for _, release := range body.Releases {
 		platform := release.Platforms[len(release.Platforms)-1]
