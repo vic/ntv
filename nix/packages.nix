119a23ec8877fa708e85bb3ce73666fc7aae11bc
Victor Borja <vborja@apache.org>
2025-03-19 18:19:46 -0600

    Update dependencies hash

--- nix/packages.nix
+++ nix/packages.nix
@@ -6,7 +6,7 @@
         pname = "nix-versions";
         src = ./..;
         version = pkgs.lib.trim (builtins.readFile ./../packages/app/VERSION);
-        vendorHash = "sha256-oxqRIk6WgZhioT8ysBehTNALlckhEiWVAKu/CLfI7dU=";
+        vendorHash = "sha256-XZXMeP+nCkohSX4+WzIxipMy+UWpgI0BIQ+5YlDfoe4=";
         meta = with pkgs.lib; {
           description = "CLI for searching nix packages versions using lazamar or nixhub, written in Go";
           homepage = "https://github.com/vic/nix-versions";
