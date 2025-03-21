27943d65d6426e9aa8a3bae40c0e14272369fa0a
Victor Borja <vborja@apache.org>
2025-03-20 21:20:48 -0600

    nix fmt

--- nix/flakeModules/nix-versions.nix
+++ nix/flakeModules/nix-versions.nix
@@ -1,33 +1,45 @@
-{inputs, nix-versions, ...}:
+{ inputs, nix-versions, ... }:
 let
 
-  nv-packages-set = pkgs: let
-  inherit (pkgs) lib;
-    toAttr = nv: let
-      name-path = lib.splitString "." nv.name;
-      attr-path = lib.splitString "." nv.attr_path;
-      pkgs' = inputs.${nv.name}.legacyPackages.${pkgs.system};
-      pkg = lib.getAttrFromPath attr-path pkgs';
-      attr = lib.setAttrByPath name-path pkg;
-    in attr;
-    attrs = lib.map toAttr nix-versions;
-    packages = lib.foldl lib.recursiveUpdate {} attrs;
-  in packages;
+  nv-packages-set =
+    pkgs:
+    let
+      inherit (pkgs) lib;
+      toAttr =
+        nv:
+        let
+          name-path = lib.splitString "." nv.name;
+          attr-path = lib.splitString "." nv.attr_path;
+          pkgs' = inputs.${nv.name}.legacyPackages.${pkgs.system};
+          pkg = lib.getAttrFromPath attr-path pkgs';
+          attr = lib.setAttrByPath name-path pkg;
+        in
+        attr;
+      attrs = lib.map toAttr nix-versions;
+      packages = lib.foldl lib.recursiveUpdate { } attrs;
+    in
+    packages;
 
-  nv-packages-list = pkgs: let
-    inherit (pkgs) lib;
-    set = nv-packages-set pkgs;
-    list = lib.map (nv: lib.getAttrFromPath (lib.splitString "." nv.name) set) nix-versions;
-  in list;
+  nv-packages-list =
+    pkgs:
+    let
+      inherit (pkgs) lib;
+      set = nv-packages-set pkgs;
+      list = lib.map (nv: lib.getAttrFromPath (lib.splitString "." nv.name) set) nix-versions;
+    in
+    list;
 
-  overlay = final: prev: nv-packages-set prev;
+  overlay = _final: prev: nv-packages-set prev;
 
-  packagesEnv = pkgs: pkgs.buildEnv {
-    name = "nix-versions";
-    paths = nv-packages-list pkgs;
-  };
+  packagesEnv =
+    pkgs:
+    pkgs.buildEnv {
+      name = "nix-versions";
+      paths = nv-packages-list pkgs;
+    };
 
-in {
+in
+{
 
   flake.lib.nix-versions = nix-versions;
   flake.overlays.default = overlay;
