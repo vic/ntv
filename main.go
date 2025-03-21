9dbb010ed76d397a62cc20ccefe3cadd91940b8c
Victor Borja <vborja@apache.org>
2025-03-21 01:00:32 -0600

    help on no args

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -8,7 +9,12 @@ import (
 )
 
 func main() {
-	err := app.NewAppArgs().ParseAndRun(os.Args[1:])
+	args := app.NewAppArgs()
+	if len(os.Args) < 2 {
+		fmt.Fprint(os.Stderr, app.AppHelp)
+		os.Exit(1)
+	}
+	err := args.ParseAndRun(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(2)
